059ccb3ff8eaffd56a626fbdee1693ea60c24e50
Vennela Mekarthi <130217673+Vennela-Mekarthi@users.noreply.github.com>
2023-10-11 10:49:28 +0530

    first commit

---
+++ Untitled-1.php
@@ -0,0 +1,262 @@
+<html>
+    <head>
+        <title>menu card</title>
+    </head>
+    <body>
+        <h1><i>menu list</i></h1>
+        <div style="display: flex;">
+        <div style="padding: 40px;">
+            <ul style="list-style-type: none;">
+            <li>vennila</li>
+            <li>strawberry</li>
+            <li>chocolate</li> 
+            <li>butter scotch</li>
+            <li>pista</li>
+            <li>sponish dellit</li> 
+            <li>black current</li>
+            <li>cherry</li>
+            <li>coshew</li> 
+            <li>flg fruit</li>
+            <li>koshmir</li>
+            <li>oman</li> 
+            <li>squdl</li>
+            <li>thamam</li>
+            <li>mango shake</li> 
+            <li>popaya</li>
+            <li>pineapple</li>
+            <li>chlkku</li> 
+            <li>Aanar</li>
+            <li>Tender Cocount</li>
+            </ul>
+        </div>
+        <div style="padding: 40px;">
+            <ul style="list-style-type: none;">
+            <li>75</li>
+            <li>75</li>
+            <li>80</li> 
+            <li>80</li>
+            <li>80</li>
+            <li>80</li> 
+            <li>80</li>
+            <li>80</li>
+            <li>90</li> 
+            <li>100</li>
+            <li>85</li>
+            <li>80</li> 
+            <li>80</li>
+            <li>80</li>
+            <li>80</li> 
+            <li>80</li>
+            <li>90</li>
+            <li>80</li> 
+            <li>80</li>
+            <li>80</li>
+            </ul>
+            </div>
+            <div style="padding: 40px;">
+            <ul style="list-style-type: none;">
+            <li>semam</li>
+            <li>badam shake</li>
+            <li>oreo shake</li> 
+            <li>cara milk nuts</li>
+            <li>til tak</li>
+            <ul>
+            <li>80</li>
+            <li>100</li>
+            <li>80</li> 
+            <li>80</li>
+            <li>80</li>
+            <li>150</li> 
+</ul>
+</ul>
+<ul>
+            <li>royal folooda</li> 
+            <li>pista folooda</li>
+            <li>butter falooda</li>
+            <li>MGS special falooda</li> 
+</ul>
+<ul>
+<li>160</li>
+            <li>170</li>
+            <li>210</li> 
+            <li>20</li>
+            <li>20</li>
+            <li>35</li> 
+</ul>
+<ul>
+            <li>fresh limes</li>
+            <li>Ginger limes</li>
+            <li>mint limes</li> 
+            <li>soda limes</li>
+            <li>grapes limes</li>
+            <li>curry leaves limes</li> 
+</ul>
+<ul  style="list-style-type: none;">
+            <li>25</li>
+            <li>30</li>
+            <li>35</li> 
+            <li>100</li>
+            <li>80</li>            
+</ul>
+<ul>
+            <li>frults salad pudding</li>
+            <li>dry fruits salad</li>
+          
+            </ul>
+            </div>
+            <div style="padding: 40px;">
+            </ul>
+            </div>
+            
+            <div style="padding: 40px;">
+            <ul style="list-style-type: none;">
+            <li>mutton chops</li>
+            <li>mutton chukka</li>
+            <li>mutton fry</li>
+            <li>mutton botl fry</li>
+            <li>fish fry</li> 
+            <li>fish curry</li>
+            <li>fish 65</li>
+            <li>fish masala</li> 
+            <li>chill fish</li>
+            <li>prowms fry</li>
+            <li>prowns masala</li> 
+            <li>prowns curry</li>
+            <li>chill prowns</li>
+            <li>parotta</li> 
+            <li>veechu paratta</li>
+            <li>open veechu parotta</li>
+            <li>veechu Egg parotta</li> 
+            <li>rice parotta</li>
+            <li>kottu parotta</li>
+            <li>mutton kolma parotta</li> 
+            <li>chicken kolma parotta</li>
+            <li>mutton murlobak poratta</li>
+            <li>chicken murlabak parrotta</li> 
+            <li>oil parotta</li>
+            <li>without oil parotta</li>
+            <li>panchu parotta</li> 
+            <li>sweet parotta</li>
+            <li>banana leaf mutton parotta</li>
+            <li>banana leaf chicken parotta</li> 
+            <li>chapathi</li>
+            <li>chicken dum parotta</li>
+            <li>mushroom dosa</li> 
+            <li>chicken dosa</li>
+            <li>mutton dosa</li>             
+             </ul>>   
+            </div>
+            <div style="padding: 40px;">
+                <ul style="list-style-type: none;">
+                    <li>190</li>
+                    <li>130</li>
+                    <li>120</li> 
+                    <li>80</li>
+                    <li>100</li>
+                    <li>100</li> 
+                    <li>100</li>
+                    <li>120</li>
+                    <li>130</li> 
+                    <li>170</li>
+                    <li>180</li>
+                    <li>180</li> 
+                    <li>180</li>
+                    <li>15</li>
+                    <li>25</li> 
+                    <li>25</li>
+                    <li>70</li>
+                    <li>25</li> 
+                    <li>70</li>
+                    <li>180</li>
+                    <li>170</li> 
+                    <li>180</li>
+                    <li>170</li>
+                    <li>25</li> 
+                    <li>20</li>
+                    <li>25</li>
+                    <li>30</li> 
+                    <li>210</li>
+                    <li>170</li>
+                    <li>40</li> 
+                    <li>300</li>
+                    <li>120</li>
+                    <li>130</li> 
+                    <li>160</li> 
+                                                                                                               
+                </ul>
+            </div>
+             <div style="padding: 40px;">
+                <ul style="list-style-type: none;">
+            <li>idyappam/paya</li>
+            <li>Appam/chicken gravy</li>
+            <li>veg manchuriyan</li> 
+            <li>gobl manchuriyan</li>
+            <li>chilli gobl manchuriyan</li>
+            <li>mushroom gravy</li> 
+            <li>mushroom manchuriyan</li>
+            <li>crispy veg</li>
+            <li>szechwan chicken</li> 
+            <li>chilli chicken</li>
+            <li>ginger chicken</li>
+            <li>dragon chicken</li> 
+            <li>shawarama roll</li>
+            <li>shawarma plate</li>
+            <li>veg fried rice</li> 
+            <li>egg fried rice</li>
+            <li>chicken fried rice</li>
+            <li>mushroom fried rice</li> 
+            <li>panner fried rice</li>
+            <li>mixed fried rice</li>
+            <li>schezwan chicken fried rice</li> 
+            <li>schezwan veg fried rice</li>
+            <li>talwanese chicken  fried rice</li>
+            <li>talwanese veg fried rice</li> 
+            <li>chicken noodles</li>
+            <li>veg noodles</li>
+            <li>egg noodles</li> 
+            <li>mixed noodles</li>
+            <li>schezwan chicken noodles</li>
+            <li>talwanese chicken noodles</li> 
+            <li>talwanese veg noodles</li>
+             
+                </ul>
+            </div>
+            <div style="padding: 40px;">
+                <ul  style="list-style-type: none;">
+            <li>80</li>
+            <li>135</li>
+            <li>100</li> 
+            <li>90</li>
+            <li>90</li>
+            <li>120</li> 
+            <li>120</li>
+            <li>120</li>
+            <li>150</li> 
+            <li>120</li>
+            <li>140</li>
+            <li>150</li> 
+            <li>80</li>
+            <li>110</li>
+            <li>80</li> 
+            <li>90</li>
+            <li>120</li>
+            <li>100</li> 
+            <li>110</li>
+            <li>140</li>
+            <li>140</li> 
+            <li>100</li>
+            <li>130</li>
+            <li>110</li> 
+            <li>110</li>
+            <li>80</li>
+            <li>90</li> 
+            <li>130</li>
+            <li>140</li>
+            <li>110</li> 
+            <li>140</li>
+            <li>100</li>            
+                </ul>
+                </div>
+            </div>
+    </body>
+</html>
---
+++ js.js
@@ -0,0 +1,8 @@
+function manipulateStyle()
+{
+    document.getElementById('ms').textContent="hellow";
+    document.getElementById('ms').style.color='blue';
+    document.getElementById('ms').style.backgroundColor='pink';
+let a=30;
+console.log(a);
+}
